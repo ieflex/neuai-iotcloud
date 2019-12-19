bc63ebe804474c1a18f7ec7683da2eca50b07464
lihui <ieflex@qq.com>
2019-12-19 18:01:57 +0800

    修改项目名称

---
+++ niot-hello/src/main/java/com/neuai/NiotCloudApplication.java
@@ -0,0 +1,17 @@
+package com.neuai;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages={"com.neuai.controller"})
+public class NiotCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiotCloudApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
