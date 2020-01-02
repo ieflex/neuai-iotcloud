7670c929e9467a4e22115fdf1e374f652d0636af
lihui <ieflex@qq.com>
2020-01-02 18:00:01 +0800

    简化例子结构

--- niot-hello/src/main/java/com/neuai/NiotCloudApplication.java
+++ niot-hello/src/main/java/com/neuai/NiotCloudApplication.java
@@ -3,10 +3,8 @@ package com.neuai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.neuai.controller"})
 public class NiotCloudApplication {
 
 	public static void main(String[] args) {
