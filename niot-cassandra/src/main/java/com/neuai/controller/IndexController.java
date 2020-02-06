a5494107fd9018731a5bb34365aa88c8d27a8bce
lihui <ieflex@qq.com>
2020-02-06 09:57:51 +0800

    modify datasource

--- niot-cassandra/src/main/java/com/neuai/controller/IndexController.java
+++ niot-cassandra/src/main/java/com/neuai/controller/IndexController.java
@@ -1,14 +1,26 @@
 package com.neuai.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.neuai.com.repository.DeviceDataRepository;
+import com.neuai.dao.DeviceData;
 
 @RestController
 @RequestMapping("/")
 public class IndexController {
+    
+    @Autowired
+    DeviceDataRepository deviceDataRepository;
 
-	@RequestMapping("index")
-	public String index() {
-		return "Hello Iot!";
-	}
+    @GetMapping(value = "/dv/{dataId}")
+    public ResponseEntity<DeviceData> getById(@PathVariable String dataId){
+    	DeviceData findOne = deviceDataRepository.findOne(dataId);
+        return new ResponseEntity<>(findOne, HttpStatus.OK);
+    }
 }
